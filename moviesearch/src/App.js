520c20d11048c0238074327660e3bbed401898a0
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-10-10 15:15:52 +0300

    added saving watched movies to localstorage

--- moviesearch/src/App.js
+++ moviesearch/src/App.js
@@ -55,13 +55,16 @@ const KEY="c14dbe0"
 
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  //const [watched, setWatched] = useState([]);
   const [isLoading,setIsLoading]= useState(false);
   const [error,setError]=useState('');
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId]= useState(null);
   
-
+  const[watched,setWatched] = useState(function (){
+    const storedValue=localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  })
   const tempQuery="interstellar"
 
   /*FETCH*/
@@ -83,12 +86,18 @@ export default function App() {
   }
 
   function handleAddWatched(movie){
-    setWatched(watched=>[...watched,movie])
+    setWatched(watched=>[...watched,movie]);
+
+    // localStorage.setItem('watched',JSON.stringify([...watched,movie]))
   }
 
   function handleDeleteWatched(id){
     setWatched((watched)=>watched.filter((movie)=>movie.imdbID !== id))
   }
+
+  useEffect(function(){
+    localStorage.setItem('watched',JSON.stringify(watched));
+  },[watched])
 
   useEffect(function(){
 
