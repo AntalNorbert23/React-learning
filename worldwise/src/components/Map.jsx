3d781ea0c76180867ec5d4f1cb514d676c062728
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-11-04 19:06:45 +0200

    added map geolocation feature

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -3,22 +3,30 @@ import styles from './Map.module.css';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext'
 import { MapContainer,TileLayer,Marker,Popup, useMap, useMapEvents } from 'react-leaflet'
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from '../components/Button'
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 function Map() {
     const {cities} = useCities();
 
     const [mapPosition,setMapPosition] = useState([40,0]);
-    const [searchParams,setSearchParams]= useSearchParams();
-
-    const mapLat = searchParams.get('lat');
-    const mapLng = searchParams.get('lng');
+    const {isLoading: isLoadingPosition,position:geolocationPosition,getPosition} =useGeolocation();
+    const [mapLat,mapLng]=useUrlPosition();
 
     useEffect(function(){
-       if(mapLat && mapLng) setMapPosition([mapLat,mapLng])
-    },[mapLat,mapLng])
+       if(mapLat && mapLng) setMapPosition([mapLat,mapLng]);
+    },[mapLat,mapLng]);
+
+    useEffect(function(){
+        if(geolocationPosition) setMapPosition([geolocationPosition.lat,geolocationPosition.lng])
+    },[geolocationPosition])
 
     return(
         <div className={styles.mapContainer} >
+            {! geolocationPosition &&( <Button type='position' onClick={getPosition}>
+                {isLoadingPosition ? 'Loading...' : 'Use your position'}
+             </Button>)}
              <MapContainer center={mapPosition} zoom={7} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
