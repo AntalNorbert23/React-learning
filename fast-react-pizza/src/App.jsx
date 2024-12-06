c61b43833ccaa19c554201b249de40f3715f9c8e
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-12-06 15:18:33 +0200

    added possibility to create order and read order

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -3,8 +3,8 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, {loader as menuLoader} from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
+import Order, {loader as orderLoader} from "./features/order/Order";
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error'
 
@@ -30,11 +30,14 @@ const router =createBrowserRouter([
       },
       {
         path: '/order/new',
-        element: <CreateOrder />
+        element: <CreateOrder />,
+        action:createOrderAction
       },
       {
         path: '/order/:orderId',
-        element: <Order />
+        element: <Order />,
+        loader: orderLoader,
+        errorElement:<Error />
       },
     ]
   },
