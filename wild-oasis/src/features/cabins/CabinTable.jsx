ca907bae25e5459f20242293b30dbf5ad91b8699
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-01-31 15:25:26 +0200

    feature of sorting cabins by multiple criteria

--- wild-oasis/src/features/cabins/CabinTable.jsx
+++ wild-oasis/src/features/cabins/CabinTable.jsx
@@ -34,6 +34,11 @@ function CabinTable(){
   if(filterValue == 'no-discount') filteredCabins = cabins.filter(cabin=>(cabin.discount) === 0);
   if(filterValue == 'with-discount') filteredCabins = cabins.filter(cabin=>(cabin.discount) !== 0);
 
+
+  const sortBy = searchParams.get('sortBy') || "startDate-asc";
+  const [field, direction] = sortBy.split('-');
+  const modifier =direction === 'asc' ? 1 : -1
+  const sortedCabins = filteredCabins.sort((a,b)=>(a[field] - b[field])*modifier);
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -45,7 +50,7 @@ function CabinTable(){
           <div>Discount</div>
           <div></div>
         </Table.Header>
-        <Table.Body data={filteredCabins} render={cabin =><CabinRow cabin={cabin} key={cabin.id}/>} />
+        <Table.Body data={sortedCabins} render={cabin =><CabinRow cabin={cabin} key={cabin.id}/>} />
       </Table>
     </Menus>
   )
