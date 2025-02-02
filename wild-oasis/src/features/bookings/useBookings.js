e79d6d8a9082dc98fbeb3c906ef1064a418a9966
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-02-02 10:21:46 +0200

    added feature of filtering bookings

--- wild-oasis/src/features/bookings/useBookings.js
+++ wild-oasis/src/features/bookings/useBookings.js
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings(){
+    const [searchParams] = useSearchParams();
+
+    //filter server side
+    const filterValue = searchParams.get("status");
+    const filter = !filterValue || filterValue === 'all' ? null : {field:'status', value: filterValue}
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings,
+        queryKey: ['bookings', filter],
+        queryFn:()=> getBookings({filter}),
       })
 
       return { isLoading, error, bookings }
