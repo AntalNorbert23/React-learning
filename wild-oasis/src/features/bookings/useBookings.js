1b1405766556b4952d3b27a01f544896315f9ef1
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-02-03 21:13:58 +0200

    added sorting and pagination to bookings

--- wild-oasis/src/features/bookings/useBookings.js
+++ wild-oasis/src/features/bookings/useBookings.js
@@ -9,9 +9,14 @@ export function useBookings(){
     const filterValue = searchParams.get("status");
     const filter = !filterValue || filterValue === 'all' ? null : {field:'status', value: filterValue}
 
+    //sort
+    const sortByRaw = searchParams.get("sortBy") || "startDate-asc";
+    const [field, direction] =sortByRaw.split('-')
+    const sortBy = {field, direction}
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ['bookings', filter],
-        queryFn:()=> getBookings({filter}),
+        queryKey: ['bookings', filter, sortBy],
+        queryFn:()=> getBookings({filter, sortBy}),
       })
 
       return { isLoading, error, bookings }
--- wild-oasis/src/services/apiBookings.js
+++ wild-oasis/src/services/apiBookings.js
@@ -7,7 +7,12 @@ export async function getBookings({filter, sortBy}){
 
 
     //Filter
-    if(filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value)
+    if(filter) query = query[filter.method || 'eq'](filter.field, filter.value)
+
+    //sort
+    if(sortBy){
+      query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'})
+    }
 
     const {data,error} = await query
 
