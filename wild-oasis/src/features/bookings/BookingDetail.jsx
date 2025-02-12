014d78219f9b8c4a7410a6dbea412147a04e8d0e
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-02-12 16:12:31 +0200

    added feature of deleting a booking

--- wild-oasis/src/features/bookings/BookingDetail.jsx
+++ wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -14,6 +14,9 @@ import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
 import { HiArrowUpOnSquare } from "react-icons/hi2";
 import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -27,6 +30,7 @@ function BookingDetail() {
   const navigate = useNavigate();
 
   const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeleting} = useDeleteBooking()
 
   if(isLoading) return <Spinner/>
 
@@ -59,6 +63,19 @@ function BookingDetail() {
             <Button icon={<HiArrowUpOnSquare />} onClick={()=>checkout(bookingId)} disabled={isCheckingOut}>
                 Check out
             </Button>}
+
+        <Modal>
+          <Modal.Open opens='delete'>
+            <Button variation='danger'>Delete booking</Button>
+          </Modal.Open>
+
+
+          <Modal.Window name='delete'>
+            <ConfirmDelete disabled={isDeleting} resourceName='booking' onConfirm={()=>deleteBooking(bookingId,{
+              onSettled: ()=> navigate(-1)
+            })}/>
+          </Modal.Window>
+        </Modal>
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
