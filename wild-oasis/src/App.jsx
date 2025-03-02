62a78973e5ba352e9cf1c7f7e8484c3f00b30e51
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-03-02 17:20:56 +0200

    added feature of dark mode

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -16,6 +16,7 @@ import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin"
 import ProtectedRoute from "./ui/ProtectedRoute";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,7 +30,7 @@ const queryClient = new QueryClient({
 
 function App(){
   return(
-    <>
+    <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyles />
@@ -76,7 +77,7 @@ function App(){
           }}
         />
       </QueryClientProvider>
-    </>
+    </DarkModeProvider>
   )
 }
 
