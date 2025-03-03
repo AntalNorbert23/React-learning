afbefff58528853ccf9d8b51fb038029d41d02ed
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-03-03 17:37:53 +0200

    added stats to the dashboard of the bookings

--- wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { useRecentBookings } from "./useRecentBookings";
 import Spinner from '../../ui/Spinner';
 import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+
+import { useCabins } from '../../features/cabins/useCabins'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -12,12 +15,19 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } =useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoadingStays} = useRecentStays();
+  const { stays, confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
+  const { cabins, isLoading: isLoading3} = useCabins();
 
-  if(isLoadingBookings || isLoadingStays) return <Spinner />
+  if(isLoadingBookings || isLoadingStays || isLoading3) return <Spinner />
 
   return (
     <StyledDashboardLayout >
+      <Stats 
+        bookings={bookings} 
+        confirmedStays={confirmedStays} 
+        numDays={numDays} 
+        cabinCount={cabins.length}
+        />
       <div>Statistics</div>
       <div>Today's activity</div>
       <div>Chart stay durations</div>
