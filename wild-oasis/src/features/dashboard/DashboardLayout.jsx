8214fb72edc73cfeacdcfa8487235e62afbecd02
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-03-06 15:15:02 +0200

    added a feature to dashboard where users can see the arriving and departing clients also being able to check them in/out

--- wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from "./Stats";
 import { useCabins } from '../../features/cabins/useCabins'
 import SalesChart from "./SalesChart";
 import DurationChart from './DurationChart'
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -17,7 +18,7 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } =useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
+  const { confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
   const { cabins, isLoading: isLoading3} = useCabins();
 
   if(isLoadingBookings || isLoadingStays || isLoading3) return <Spinner />
@@ -30,7 +31,7 @@ function DashboardLayout() {
         numDays={numDays} 
         cabinCount={cabins.length}
         />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays}/>
       <SalesChart bookings={bookings} numDays={numDays}/>
     </StyledDashboardLayout>
