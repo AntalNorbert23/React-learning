f474f8c307f94525a1ebb732e5ee6007252e50a4
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-03-20 15:51:10 +0200

    added nested layouts and further simple elements of the site

--- wild-oasis-website/app/layout.js
+++ wild-oasis-website/app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({children}){
     <html lang="en">
         <body className={`${josefin.className} antialiased relative bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
             <Header />
-            <div className="flex-1 px-8 py-12">
-              <main className="max-w-7xl mx-auto">
+            <div className="flex-1 px-8 py-12 grid">
+              <main className="max-w-7xl mx-auto w-full">
                 {children}
               </main>
             </div>
