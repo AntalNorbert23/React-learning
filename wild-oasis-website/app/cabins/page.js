77e3a073685ee631b9511cc11c3074b0f041c6c5
AntalNorbert23 <anorbertcsaba@yahoo.com>
2025-04-02 15:01:58 +0300

    added feature of filtering cabins

--- wild-oasis-website/app/cabins/page.js
+++ wild-oasis-website/app/cabins/page.js
@@ -1,16 +1,19 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 
+export const revalidate = 15;
 
 export const metadata = {
     title: 'Cabins'
   }
 
-export default function Page() {
+export default function Page({searchParams}) {
   // CHANGE
  
+  const filter = searchParams?.capacity ?? "all";
 
   return (
     <div>
@@ -21,12 +24,16 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-        <Suspense fallback= {<Spinner />}>
-          <CabinList />
+
+        <div className="flex justify-end mb-8">
+          <Filter />
+        </div>
+        <Suspense fallback= {<Spinner />} key={filter}>
+          <CabinList filter={filter}/>
         </Suspense>
     </div>
   );
